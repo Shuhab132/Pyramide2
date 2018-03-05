6ea9415430a6f7d998ee66f0a6f9b477bf70d757
jonathanka <j.kjaer.andersen@gmail.com>
2018-03-05 16:52:22 +0100

    PyramideV1
    - Added all.

--- src/pyramide2/Boks3.java
+++ src/pyramide2/Boks3.java
@@ -1,41 +1,68 @@
-package pyramide2;
-/** En boks med en længde, bredde og højde */
-public class Boks3
-{
-	private double længde;
-	private double bredde;
-	private double højde;
+package kapitel_04;
 
-	/** konstruktør der opretter en standardboks */
-	public Boks3()
-	{
-		System.out.println("Standardboks oprettes");
-		længde = 10.0;
-		bredde = 10.0;
-		højde  = 10.0;
-	}
+/**
+ * En boks med en længde, bredde og højde
+ */
+public class Boks3 {
 
-	/** en anden konstruktør der får bredde, højde og længde */
-	public Boks3(double lgd, double b, double h)
-	{
-		if (lgd>0 && b>0 && h>0)
-		{
-			System.out.println("Boks oprettes med lgd="+lgd+" b="+b+" h="+h);
-			længde = lgd;
-			bredde = b;
-			højde  = h;
-		} else {
- 			System.out.println("Ugyldige mål. Bruger standardmål.");
-			længde = 10.0;
-			bredde = 10.0;
-			højde  = 10.0;
-		}
-	}
+    private double længde;
+    private double bredde;
+    private double højde;
+    private double masse;
+    private double side1;
+    private double side2;
 
-	/** udregner boksens rumfang */
-	public double volumen()
-	{
-		double vol = længde*bredde*højde;
-		return vol;
-	}
+    /**
+     * konstruktør der opretter en standardboks
+     */
+    public Boks3() {
+        System.out.println("Standardboks oprettes");
+        længde = 8.0;
+        bredde = 45.0;
+        højde = 4.0;
+        masse = 3.4;
+        side1 = 35;
+        side2 = 36;
+    }
+
+    /**
+     * en anden konstruktør der får bredde, højde og længde
+     */
+    public Boks3(double lgd, double b, double h) {
+        if (lgd > 0 && b > 0 && h > 0) {
+            System.out.println("Boks oprettes med lgd=" + lgd + " b=" + b + " h=" + h);
+            længde = lgd;
+            bredde = b;
+            højde = h;
+        } else {
+            System.out.println("Ugyldige mål. Bruger standardmål.");
+            længde = 10.0;
+            bredde = 10.0;
+            højde = 10.0;
+        }
+    }
+
+    public double pyramide() {
+        double pyra = (side1 * side2 * højde) / 4;
+        return pyra;
+    }
+
+    /*
+     * udregner boksens rumfang
+     * @return 
+     */
+    public double volumen() {
+        double vol = længde * bredde * højde;
+        return vol;
+    }
+
+    public double massefylde() {
+        double mass = (længde * bredde * højde) / masse;
+        return mass;
+    }
+
+    public double vægt() {
+        double vægt = (længde * bredde * højde) * masse;
+        return vægt;
+    }
 }
